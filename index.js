1e1a34bef406f6e6c5d69a359ecd190f5d4d20bf
vladislavkostromin <61909342+vladislavkostromin@users.noreply.github.com>
2021-01-04 17:10:54 +0500

    js selected + style correct

--- index.js
+++ index.js
@@ -2,5 +2,16 @@ import {Select} from './select/select';
 import './select/styles.scss';
 
 const select = new Select('#select', {
-    
-})
+    placeholder: 'Выберите элемент',
+    data: [
+        {id: '1', value: 'React'},
+        {id: '2', value: 'Angular'},
+        {id: '3', value: 'Vue'},
+        {id: '4', value: 'React Native'},
+        {id: '5', value: 'Next'},
+        {id: '6', value: 'Nest'},
+      ]
+});
+
+
+window.s = select
